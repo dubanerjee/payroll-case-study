fdcfff495f0b5bfc990dcf3ab3620fd17571176c
dubanerjee <dbanerje1979+github@gmail.com>
2017-07-23 17:17:09 -0500

    Simple Spring Boot project with Gradle and Kotlin

---
+++ src/main/kotlin/org/dubanerjee/demos/payroll/app/Application.kt
@@ -0,0 +1,12 @@
+package org.dubanerjee.demos.payroll.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.SpringApplication
+
+@SpringBootApplication
+class Application {
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
---
+++ src/main/kotlin/org/dubanerjee/demos/payroll/app/HelloController.kt
@@ -0,0 +1,10 @@
+package org.dubanerjee.demos.payroll.app
+
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.GetMapping
+
+@RestController
+class HelloController {
+    @GetMapping("/hello")
+    fun index(): String = "Hello World!"
+}
